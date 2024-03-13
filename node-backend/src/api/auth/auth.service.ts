556ae4831c743c5698caead6d217c7e2754d035d
Divyanshu Yadav <divyanshu.1810@gmail.com>
2024-03-13 22:02:55 +0530

    feat: add forgot password

--- node-backend/src/api/auth/auth.service.ts
+++ node-backend/src/api/auth/auth.service.ts
@@ -1,13 +1,10 @@
 import database from "../../loaders/database";
-import { ForgotPassword, Signup } from "./auth.schema";
+import { ForgotPassword, Signup, VerifyOTP } from "./auth.schema";
 import bcrypt from 'bcrypt';
 
 export const handleSignUp = async (data: Signup) => {
     const userCollection = await (await database()).collection('user');
     const user = await userCollection.findOne({ email: data.email });
-    if (user) {
-        throw new Error('User already exists');
-    }
     const saltRounds = 10;
     const hash = await bcrypt.hash(data.password, saltRounds);
     await userCollection.insertOne(
@@ -20,7 +17,9 @@ export const handleSignUp = async (data: Signup) => {
     );
 };
 
-export const handleVerifyOTP = async () => { };
+export const handleVerifyOTP = async (data: VerifyOTP) => {
+    //TODO: implement this function
+};
 
 export const handleForgotPassword = async (data: ForgotPassword) => {
     const userCollection = await (await database()).collection('user');
