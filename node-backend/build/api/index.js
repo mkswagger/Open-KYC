8cee1b37804f3c8b15c1b2832e73565b0e457d69
Divyanshu Yadav <divyanshu.1810@gmail.com>
2024-03-13 20:05:50 +0530

    chore: fix mongo error

---
+++ node-backend/build/api/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+exports.default = () => {
+    const app = (0, express_1.Router)();
+    //TODO: add routes here...
+    return app;
+};
+//# sourceMappingURL=index.js.map
