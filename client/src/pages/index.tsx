8f72793edaac2cc5f4ad0318c35334e321c31a1c
Divyanshu Yadav <divyanshu.yadav@hyrecruitai.com>
2024-03-13 18:00:04 +0530

    feat: shadcn init

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,22 @@
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const fontSans = FontSans({
+    subsets: ["latin"],
+    variable: "--font-sans",
+});
+
 export default function Home() {
-  return <main>Heisenberg</main>;
+    return (
+        <main
+            className={cn(
+                "min-h-screen bg-background font-sans antialiased",
+                fontSans.variable
+            )}
+        >
+            <Button>Click me</Button>
+            Heisenberg
+        </main>
+    );
 }
