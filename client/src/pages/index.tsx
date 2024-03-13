b68531ab59bfd93d7641154827d775dba98fe6cd
Divyanshu Yadav <divyanshu.1810@gmail.com>
2024-03-13 18:07:32 +0530

    chore: shadcn init

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,4 @@
 import { Inter as FontSans } from "next/font/google";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 
 const fontSans = FontSans({
     subsets: ["latin"],
@@ -8,15 +6,5 @@ const fontSans = FontSans({
 });
 
 export default function Home() {
-    return (
-        <main
-            className={cn(
-                "min-h-screen bg-background font-sans antialiased",
-                fontSans.variable
-            )}
-        >
-            <Button>Click me</Button>
-            Heisenberg
-        </main>
-    );
+    return <main>Heisenberg</main>;
 }
