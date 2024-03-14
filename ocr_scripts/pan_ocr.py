d25aedb9437240be49a2ed4d63c2a3e0ee783dc7
mkswagger <mathangykrishna16@gmail.com>
2024-03-14 14:12:44 +0530

    new ocr function for pan

---
+++ ocr_scripts/pan_ocr.py
@@ -0,0 +1,54 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import re
+#Tesseract Library
+import pytesseract
+
+import cv2
+import matplotlib.pyplot as plt
+from PIL import Image
+import os
+
+### Make prettier the prints ###
+from colorama import Fore, Style
+c_ = Fore.CYAN
+m_ = Fore.MAGENTA
+r_ = Fore.RED
+b_ = Fore.BLUE
+y_ = Fore.YELLOW
+g_ = Fore.GREEN
+w_ = Fore.WHITE
+
+import warnings
+warnings.filterwarnings(action='ignore') 
+
+def ExtractDetails(image_path):
+    text = pytesseract.image_to_string(Image.open(image_path), lang = 'eng')
+    text = text.replace("\n", " ")
+    text = text.replace("  ", " ")
+    regex_DOB = re.compile('\d{2}[-/]\d{2}[-/]\d{4}')
+    regex_num = re.compile('[A-Z]{5}[0-9]{4}[A-Z]{1}')
+    
+    image = cv2.imread(os.path.join(image_path))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    plt.imshow(image)
+    plt.axis("off")  
+    
+    if len(regex_num.findall(text)) == 0:
+        print(f'{y_}Blurry Image for tesseract. Input new clear image for viewing pan card number !!!')
+        print(Style.RESET_ALL)
+    else:
+        print("Pan Card Number : ", regex_num.findall(text)[0])
+        
+    print('=================================')
+    
+    if len(regex_DOB.findall(text)) == 0:
+        print(f'{y_}Blurry Image for tesseract. Input new clear image for viewing DATE OF BIRTH !!!')
+        print(Style.RESET_ALL)
+    else:
+        print("DATE OF BIRTH :   ", regex_DOB.findall(text)[0])
+        
+    print('=================================')
+
+
+ExtractDetails('pancard_try.jpeg')
