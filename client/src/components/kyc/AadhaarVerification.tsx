e6b8a678510d6b23a9a66aed98d33b91d278714e
itaintkevin <kpjtofficial@gmail.com>
2024-03-14 15:19:19 +0530

    fix: UI fixes

--- client/src/components/kyc/AadhaarVerification.tsx
+++ client/src/components/kyc/AadhaarVerification.tsx
@@ -3,10 +3,10 @@ import { Button } from "../ui/button";
 
 export default function AadhaarVerification() {
   return (
-    <div>
+    <div className="text-center my-10">
       <h2>Here are the details we fetched from your aadhaar card.</h2>
       {}
-      <Button>Verify & Continue</Button>
+      <Button className="my-2">Verify & Continue</Button>
     </div>
   );
 }
