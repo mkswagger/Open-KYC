f9b466d5efc2c845eb90acc91819163c49b7d99c
itaintkevin <kpjtofficial@gmail.com>
2024-03-14 15:17:57 +0530

    feat: Added aadhaar verification as a new step

---
+++ client/src/components/kyc/AadhaarVerification.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "../ui/button";
+
+export default function AadhaarVerification() {
+  return (
+    <div>
+      <h2>Here are the details we fetched from your aadhaar card.</h2>
+      {}
+      <Button>Verify & Continue</Button>
+    </div>
+  );
+}
--- client/src/pages/kyc.tsx
+++ client/src/pages/kyc.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Form from "@/components/kyc/Form";
 import CaptureFrame from "@/components/kyc/CaptureFrame";
 import VerifyAndComplete from "@/components/kyc/VerifyAndComplete";
+import AadhaarVerification from "@/components/kyc/AadhaarVerification";
 
 export default function kyc() {
   const [currentStep, setCurrentStep] = useState(1);
@@ -43,6 +44,16 @@ export default function kyc() {
           <span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
             <span className="me-2">2</span>
             <span className="hidden sm:inline-flex sm:ms-2">
+              Aadhaar Verification
+            </span>
+          </span>
+        </li>
+        <li
+          className={`flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700 ${currentStep === 2 ? "text-blue-600 dark:text-blue-500" : "text-gray-500 dark:text-gray-400"}`}
+        >
+          <span className="flex items-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
+            <span className="me-2">3</span>
+            <span className="hidden sm:inline-flex sm:ms-2">
               Video Verification
             </span>
           </span>
@@ -50,15 +61,16 @@ export default function kyc() {
         <li
           className={`flex items-center ${currentStep === 3 ? "text-blue-600 dark:text-blue-500" : "text-gray-500 dark:text-gray-400"}`}
         >
-          <span className="me-2">3</span>
+          <span className="me-2">4</span>
           KYC Confirmation
         </li>
       </ol>
 
       {/* Render Form or VideoFeed based on currentStep */}
       {currentStep === 1 && <Form onNextStep={handleNextStep} />}
-      {currentStep === 2 && <CaptureFrame onNextStep={handleNextStep} />}
-      {currentStep === 3 && <VerifyAndComplete />}
+      {currentStep === 2 && <AadhaarVerification onNextStep={handleNextStep} />}
+      {currentStep === 3 && <CaptureFrame onNextStep={handleNextStep} />}
+      {currentStep === 4 && <VerifyAndComplete />}
     </div>
   );
 }
